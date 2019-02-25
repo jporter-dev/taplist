62bb87bf51ed4332f72a3589c4cf40927d8c5bda
Josh Porter <joshporter1@gmail.com>
2019-02-24 23:56:19 -0500

    Swapped out axios/cheerio for puppeteer.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,37 +1,31 @@
 const yaml = require('js-yaml');
-const request = require('axios');
-const cheerio = require('cheerio');
+const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 function main() {
   // Get document, or throw exception on error
   try {
     let data = yaml.safeLoad(fs.readFileSync('./sites.yml', 'utf8'));
-    data.sites.map(site => parseSite(site))
+    void(async () => {
+      await data.sites.map(site => parseSite(site))
+    })()
   } catch (e) {
     console.log(e);
   }
 }
 
-function parseSite(site) {
-  request(site.url)
-    .then((html) => {
-      extractData(html.data, site)
-      .then(list => {
-        console.log(site.name, list.length)
-      })
-    })
-}
+async function parseSite(site) {
+  let browser = await puppeteer.launch()
+  let page = await browser.newPage()
 
-function extractData(html, site) {
-  return new Promise(function (resolve, reject) {
-    const $ = cheerio.load(html)
-    let beers = $(site.selector).map(function (i, el) {
-      return $(this).text().trim();
-    }).get();
-
-    resolve(beers)
-  })
+  await page.goto(site.url)
+  await page.waitForSelector(site.selector);
+  const beers = await page.evaluate((selector) => {
+    const list = Array.from(document.querySelectorAll(selector));
+    return list.map(item => item.innerText.trim());
+  }, site.selector);
+  await browser.close()
+  console.log(site.name, beers.length)
 }
 
 main()
