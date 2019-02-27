164a5f63e95a40bcf80b6f6a72d63f4705180dce
Josh Porter <joshua_porter@mcafee.com>
2019-02-27 15:21:01 -0500

    Added name filtering to scraper

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,95 +1,64 @@
 #!/usr/bin/env node
 
-const yaml = require('js-yaml');
-const axios = require('axios');
-const puppeteer = require('puppeteer');
-const path = require('path');
-const fs = require('fs');
-
+const yaml = require("js-yaml");
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
 
 function main() {
   // Get document, or throw exception on error
   try {
-    let data = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, 'sites.yml'), 'utf8'));
-    let db = []
-    void(async () => {
+    let data = yaml.load(
+      fs.readFileSync(path.resolve(__dirname, "sites.yml"), "utf8")
+    );
+    let db = [];
+    void (async () => {
       let promises = data.sites.map(site => {
         return new Promise(resolve => {
-          parseSite(site)
-            .then(beers => {
-              db = db.concat(beers)
-              resolve()
-            })
-        })
-      })
-      Promise.all(promises)
-        .then(() => fs.writeFileSync(path.resolve(__dirname, '../public/taplist.json'), JSON.stringify(db)))
-    })()
+          parseSite(site).then(beers => {
+            db = db.concat(beers);
+            resolve();
+          });
+        });
+      });
+      Promise.all(promises).then(() =>
+        fs.writeFileSync(
+          path.resolve(__dirname, "../public/taplist.json"),
+          JSON.stringify(db)
+        )
+      );
+    })();
   } catch (e) {
     console.log(e);
   }
 }
 
 async function parseSite(site) {
-  let browser = await puppeteer.launch()
-  let page = await browser.newPage()
-  await page.goto(site.url)
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+  await page.goto(site.url);
   await page.waitForSelector(site.selector);
-  let beers = await page.evaluate((selector) => {
-    const list = Array.from(document.querySelectorAll(selector));
-    return list.map(item => item.innerText.trim().replace(/\s+/g, " "));
+  let beers = await page.evaluate(selector => {
+    return Array.from(document.querySelectorAll(selector)).map(beer =>
+      beer.innerText.trim()
+    );
   }, site.selector);
-  await browser.close()
+  await browser.close();
 
   beers = beers.map(beer => {
+    beer = beer.replace(/\s+/g, " ");
+    // process with namefilter if exists
+    if (site.hasOwnProperty("namefilter")) {
+      beer = site.namefilter(beer);
+    }
     return {
       name: beer,
       location: site.name
-    }
-  })
+    };
+  });
   return new Promise(resolve => {
-    resolve(beers)
-  })
+    resolve(beers);
+  });
 }
 
-
-function ingest(site, beers) {
-  beers.map(beer => {
-    const mutation = `mutation {
-  upsertLocation(
-    where: { name: "${site.name}" }
-    create: { name: "${site.name}", url: "${site.url}" }
-    update: { name: "${site.name}" }
-  ) {
-    id
-  }
-  upsertBeer(
-    where: { name: "${beer}" }
-    create: {
-      name: "${beer}"
-      locations: { connect: [{ name: "${site.name}" }] }
-    }
-    update: {
-      locations: { connect: [{ name: "${site.name}" }] }
-    }
-  ) {
-    id
-  }
-}`
-    axios({
-        url: 'http://localhost:4466',
-        method: 'POST',
-        data: {
-          query: mutation
-        }
-      })
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log("ERROR!!!!!!!!!!!", error);
-      });
-  })
-}
-
-main()
+main();
