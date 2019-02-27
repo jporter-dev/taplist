9854a5d8848d45ab0f6c7ebd9c5e2ae799182077
Josh Porter <joshporter1@gmail.com>
2019-02-26 21:21:51 -0500

    Updated scraper and modified Prisma schema

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const yaml = require('js-yaml');
+const axios = require('axios');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
@@ -24,10 +25,50 @@ async function parseSite(site) {
   await page.waitForSelector(site.selector);
   const beers = await page.evaluate((selector) => {
     const list = Array.from(document.querySelectorAll(selector));
-    return list.map(item => item.innerText.trim());
+    return list.map(item => item.innerText.trim().replace(/\s+/g, " "));
   }, site.selector);
   await browser.close()
-  console.log(site.name, beers.length)
+  ingest(site, beers)
+}
+
+
+function ingest(site, beers) {
+  beers.map(beer => {
+    const mutation = `mutation {
+  upsertLocation(
+    where: { name: "${site.name}" }
+    create: { name: "${site.name}", url: "${site.url}" }
+    update: { name: "${site.name}" }
+  ) {
+    id
+  }
+  upsertBeer(
+    where: { name: "${beer}" }
+    create: {
+      name: "${beer}"
+      locations: { connect: [{ name: "${site.name}" }] }
+    }
+    update: {
+      locations: { connect: [{ name: "${site.name}" }] }
+    }
+  ) {
+    id
+  }
+}`
+    axios({
+        url: 'http://localhost:4466',
+        method: 'POST',
+        data: {
+          query: mutation
+        }
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log("ERROR!!!!!!!!!!!", error);
+      });
+  })
 }
 
 main()
