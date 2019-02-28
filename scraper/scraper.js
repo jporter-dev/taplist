e04237edb5d0b8e88f2b682426c2afdaa16a6b18
Josh Porter <joshua_porter@mcafee.com>
2019-02-28 15:18:44 -0500

    Add untappd scraping via puppeteer

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,77 +1,132 @@
 #!/usr/bin/env node
 
-const yaml = require("js-yaml");
 const puppeteer = require("puppeteer");
 const isWsl = require("is-wsl");
-const process = require("process");
-const path = require("path");
-const fs = require("fs");
+// const yaml = require("js-yaml");
+// const path = require("path");
+// const fs = require("fs");
 
-function main() {
-  // Get document, or throw exception on error
-  try {
-    let data = yaml.load(
-      fs.readFileSync(path.resolve(__dirname, "config.yml"), "utf8")
-    );
-    let db = [];
-    void (async () => {
-      let promises = data.sites.map(site => {
-        return new Promise(resolve => {
-          parseSite(site)
-            .then(beers => {
-              db = db.concat(beers);
-              resolve();
-            })
-            .catch(e => {
-              console.log(`${site.name}\n`, e);
-              process.exit(1);
-            });
-        });
-      });
-      Promise.all(promises).then(() =>
-        fs.writeFileSync(
-          path.resolve(__dirname, "../public/taplist.json"),
-          JSON.stringify({ taplist: db, last_updated: Date.now() })
-        )
-      );
-    })();
-  } catch (e) {
-    console.log(e);
+// function main() {
+//   // Get document, or throw exception on error
+//   try {
+//     let data = yaml.load(
+//       fs.readFileSync(path.resolve(__dirname, "config.yml"), "utf8")
+//     );
+//     let db = [];
+//     void(async () => {
+//       let promises = data.sites.map(site => {
+//         return new Promise(resolve => {
+//           parseSite(site)
+//             .then(beers => {
+//               db = db.concat(beers);
+//               resolve();
+//             })
+//             .catch(e => {
+//               console.log(`${site.name}\n`, e);
+//               process.exit(1);
+//             });
+//         });
+//       });
+//       Promise.all(promises).then(() =>
+//         fs.writeFileSync(
+//           path.resolve(__dirname, "../public/taplist.json"),
+//           JSON.stringify({
+//             taplist: db,
+//             last_updated: Date.now()
+//           })
+//         )
+//       );
+//     })();
+//   } catch (e) {
+//     console.log(e);
+//   }
+// }
+
+// async function parseSite(site) {
+//   let opts = isWsl ?
+//     {
+//       executablePath: "/usr/bin/chromium-browser",
+//       args: ["--no-sandbox", "--disable-setuid-sandbox"]
+//     } :
+//     {};
+//   let browser = await puppeteer.launch(opts);
+//   let page = await browser.newPage();
+//   await page.goto(site.url);
+//   await page.waitForSelector(site.selector);
+//   let beers = await page.evaluate(selector => {
+//     return Array.from(document.querySelectorAll(selector)).map(beer =>
+//       beer.innerText.trim()
+//     );
+//   }, site.selector);
+//   await browser.close();
+
+//   beers = beers.map(beer => {
+//     beer = beer.replace(/\s+/g, " ");
+//     // process with namefilter if exists
+//     if (site.hasOwnProperty("namefilter")) {
+//       beer = site.namefilter(beer);
+//     }
+//     return {
+//       name: beer,
+//       location: site.name
+//     };
+//   });
+//   return new Promise(resolve => {
+//     resolve(beers);
+//   });
+// }
+
+function getUniques(all) {
+  all = all || false;
+  let user = {
+    username: "ididnotbiteyou"
+  };
+  if (!all) {
+    fetchUntappd(user);
   }
 }
 
-async function parseSite(site) {
+async function fetchUntappd(user) {
   let opts = isWsl
     ? {
         executablePath: "/usr/bin/chromium-browser",
         args: ["--no-sandbox", "--disable-setuid-sandbox"]
       }
     : {};
+  let url = `https://untappd.com/user/${user.username}/beers`;
+
+  console.log("Fetching untappd beers at " + url);
   let browser = await puppeteer.launch(opts);
   let page = await browser.newPage();
-  await page.goto(site.url);
-  await page.waitForSelector(site.selector);
-  let beers = await page.evaluate(selector => {
-    return Array.from(document.querySelectorAll(selector)).map(beer =>
-      beer.innerText.trim()
+  await page.goto(url);
+  console.log("Navigating...");
+  let buttonSelector = ".more-list-items";
+  console.log(`Waiting for selector [${buttonSelector}]...`);
+  await page.waitForSelector(buttonSelector);
+  loadMore(page, buttonSelector);
+
+  let beers = await page.evaluate(() => {
+    return Array.from(document.querySelectorAll(".beer-item")).map(
+      item =>
+        `${item.querySelector("brewery").innerText.trim()} ${item
+          .querySelector(".name")
+          .innerText.trim()}`
     );
-  }, site.selector);
+  });
   await browser.close();
 
-  beers = beers.map(beer => {
-    beer = beer.replace(/\s+/g, " ");
-    // process with namefilter if exists
-    if (site.hasOwnProperty("namefilter")) {
-      beer = site.namefilter(beer);
-    }
-    return {
-      name: beer,
-      location: site.name
-    };
-  });
-  return new Promise(resolve => {
-    resolve(beers);
-  });
+  console.log(beers);
 }
 
-main();
+async function loadMore(page, buttonSelector) {
+  console.log("clicking load more...");
+  await page.click(buttonSelector);
+  try {
+    await page.waitForSelector(buttonSelector);
+  } catch (error) {
+    console.log("End of list.");
+    return;
+  }
+}
+
+getUniques();
