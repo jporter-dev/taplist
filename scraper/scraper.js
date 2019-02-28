971931571b85286c5557cbac7745787f43fca9ae
Josh Porter <joshporter1@gmail.com>
2019-02-28 00:24:18 -0500

    Redid scraper delay

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -49,39 +49,40 @@ function main() {
 }
 
 function getUntapped(beers) {
-  let promises = beers.map((beer, idx1) => {
-    USERS.map((user, idx2) => {
-      console.log(user);
+  let promises = [];
+  beers.forEach((beer, idx1) => {
+    USERS.forEach((user, idx2) => {
+      idx1 += 1;
+      idx2 += 1;
       let url = encodeURI(
         `https://untappd.com/user/${user.name}/beers?sort=date&q=${beer.name}`
       );
       beer.unique = beer.unique || [];
 
-      return new Promise((resolve, reject) => {
-        let delay = 100 * (idx1 * idx1) * idx2;
-        console.log(delay);
-        setTimeout(
-          () =>
-            axios
-              .get(url)
-              .then(response => {
-                let unique = parseUntapped(response.data);
-                if (unique) beer.unique.push(user.name);
-                resolve(beer);
-              })
-              .then(res => resolve(res))
-              .catch(err => {
-                console.log(err);
-                reject(err);
-              }),
-          delay
-        );
-      });
+      promises.push(
+        new Promise((resolve, reject) => {
+          let delay = 1000 * (idx1 * idx1) + idx2 * 1000;
+          setTimeout(
+            () =>
+              axios
+                .get(url)
+                .then(response => {
+                  let unique = parseUntapped(response.data);
+                  if (unique) beer.unique.push(user.name);
+                  resolve(beer);
+                })
+                .then(res => resolve(res))
+                .catch(err => {
+                  console.log(err.response.status, err.response.headers);
+                  reject(err);
+                }),
+            delay
+          );
+        })
+      );
     });
   });
-  Promise.all(promises).then(() => {
-    console.log(beers);
-  });
+  return Promise.all(promises);
 }
 
 function parseUntapped(html) {
@@ -129,80 +130,4 @@ async function parseSite(site) {
 
 // main();
 
-getUntapped([
-  {
-    name: "Austin Eastciders Blood Orange Cider"
-  },
-  {
-    name: "1623 Brewing Pilsner"
-  },
-  {
-    name: "1623 IPA"
-  },
-  {
-    name: "3 Stars Funkerdome #2"
-  },
-  {
-    name: "3rd Wave Juice Box Cran/Or."
-  },
-  {
-    name: "3rd Wave Juice Box Series: Peaches & Cream"
-  },
-  {
-    name: "3rd Wave Kohana Choc/Cherry Stout"
-  },
-  {
-    name: "Abita Amber"
-  },
-  {
-    name: "Abita Rootbeer (Non Alcoholic)"
-  },
-  {
-    name: "Abita Strawberry Harvest Lager"
-  },
-  {
-    name: "Achouffe N'Ice Chouffe"
-  },
-  {
-    name: "Adroit Theory Elegy(Fear) NE DIPA"
-  },
-  {
-    name: "Adroit Theory IX (fedaykin Ed.) DIPA"
-  },
-  {
-    name: "Adroit Theory IX(deathstill Ed.) DIPA"
-  },
-  {
-    name: "Allagash Bon Vieux Temps"
-  },
-  {
-    name: "Allagash Bon Vieux Temps"
-  },
-  {
-    name: "Allagash Tripel"
-  },
-  {
-    name: "Allagash White"
-  },
-  {
-    name: "Allagash White Ale"
-  },
-  {
-    name: "Alvinne Wild West w/French Plums"
-  },
-  {
-    name: "Austin East Blood Orange Cider"
-  },
-  {
-    name: "Austin Eastciders Blood Orange Cider"
-  },
-  {
-    name: "Avery The Kaiser"
-  },
-  {
-    name: "Backroom Lemon Basil Wheat"
-  },
-  {
-    name: "Badische Staatsbrauerei Rothaus Rothaus Pils / Tannen Zäpfle"
-  }
-]);
+main();
