16a2fa416ba794ce1317ef9861da98678ecc1920
Joshua Porter <joshporter1@gmail.com>
2019-06-27 20:48:13 -0400

    Feature/untappd auth (#1)
    * Trying out untappd auth feature
    
    * Temporarily disable scraper
    
    * Trying out untappd auth feature
    
    * Updated redirects
    
    * Updated redirects
    
    * Updated redirects
    
    * Updated redirects
    
    * Added untappd auth and logout button
    
    * Added untappd auth and logout button
    
    * Added untappd auth and logout button
    
    * Swapped sidebar click events to router links
    
    * Changed row expansion from scraping page to using API
    
    * Added user checkin feed and added more data to table expansion
    
    * Added error message for unauthenticated users
    
    * Updated netlify.toml
    
    * Updated netlify.toml
    
    * Updated netlify.toml
    
    * Updated netlify.toml
    
    * Updated netlify.toml
    
    * Updated netlify.toml

--- src/store.js
+++ src/store.js
@@ -25,7 +25,9 @@ const store = new Vuex.Store({
     users: null,
     leaderboard: null,
     last_updated: null,
-    last_updated_timestamp: null
+    last_updated_timestamp: null,
+    untappd: null,
+    untappd_user: null
   },
   mutations: {
     SET_DATA(state, data) {
@@ -43,6 +45,16 @@ const store = new Vuex.Store({
     },
     SET_SEARCH(state, payload) {
       state.search = payload;
+    },
+    SET_UNTAPPD(state, payload) {
+      state.untappd = payload;
+    },
+    SET_UNTAPPD_USER(state, payload) {
+      state.untappd_user = payload;
+    },
+    LOGOUT(state) {
+      state.untappd = null;
+      state.untappd_user = null;
     }
   },
   actions: {
@@ -50,6 +62,12 @@ const store = new Vuex.Store({
       fetch("/taplist.json", {cache: "no-store"})
         .then(response => response.json())
         .then(json => store.commit("SET_DATA", json));
+    },
+    getUntappdUser(store) {
+      const base = "https://api.untappd.com";
+      fetch(`${base}/v4/user/info?access_token=${store.state.untappd}`)
+        .then(response => response.json())
+        .then(json => store.commit("SET_UNTAPPD_USER", json.response.user));
     }
   }
 });
